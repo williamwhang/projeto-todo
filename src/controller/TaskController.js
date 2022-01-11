461fabd593bbd0c3f1d0a1dd58fbe612e9833d76
williamwhang <whang.william@gmail.com>
2022-01-11 17:21:21 -0300

    Exibindo uma tarefa específica

--- src/controller/TaskController.js
+++ src/controller/TaskController.js
@@ -37,6 +37,18 @@ class TaskController {
             });
     }
 
+    async show(req, res) {
+        await TaskModel.findById(req.params.id)
+        .then(response => {
+            if(response)
+            return res.status(200).json(response);
+            else
+            return res.status(404).json({ error: 'tarefa não encontrada'});
+        })
+        .catch(error => {
+            return res.status(500).json(error);
+        })
+    }
 }
 
 module.exports = new TaskController();
--- src/routes/TaskRoutes.js
+++ src/routes/TaskRoutes.js
@@ -8,8 +8,8 @@ const MacaddressValidation = require('../middlewares/MacaddressValidation');
 
 router.post('/', TaskValidation, TaskController.create);
 router.put('/:id', TaskValidation, TaskController.update);
-const MacaddressValidation = require('../middlewares/MacaddressValidation');
-const MacaddressValidation = require('../middlewares/MacaddressValidation');
+route.get('/:id', TaskController.show);
+
 route.get('/filter/all', MacaddressValidation, TaskController.all);
 
 module.exports = router;
