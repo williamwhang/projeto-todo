71a08d3183136a450536eddeff8fe9e219ba389e
williamwhang <whang.william@gmail.com>
2022-01-11 17:03:08 -0300

    Arrumando

--- src/controller/TaskController.js
+++ src/controller/TaskController.js
@@ -31,11 +31,11 @@ class TaskController {
             .sort('when')
             .then(response => {
                 return res.status(200).json(response);
+            })
             .catch(error => {
                 return res.status(500).json(error);
             });
-    })
-}
+    }
 
 }
 
